7802c9018aedd707778a7d2efd9b8fd0d5cabb2d
Shawan Das <86365217+Shawan-Das@users.noreply.github.com>
2023-08-08 17:05:19 +0600

    Problem with WebApplication1-datanot found

---
+++ WebApplication1/WebApplication1/Models/PostgresContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+namespace WebApplication1.Models
+{
+	public class PostgresContext : DbContext
+	{
+		public PostgresContext(DbContextOptions<PostgresContext> options)
+			: base(options)
+		{ }
+		public virtual DbSet<Employee> employes { get; set; } = null!;
+		public virtual DbSet<Department> departments { get; set; } = null!;
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			if(!optionsBuilder.IsConfigured)
+			{
+				optionsBuilder.UseNpgsql("Host=localhost;Database=testdb;Port=8080;Username= postgres;Password=1234");
+			}
+		}
+	}
+}
